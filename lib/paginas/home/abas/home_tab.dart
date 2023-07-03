5a1e0351b804dec49f39d4fb205bd9fdf9bedc11
Junior <vgfonsecajunior@gmail.com>
2023-07-02 23:43:03 -0300

    add botão configuraçãp

--- lib/paginas/home/abas/home_tab.dart
+++ lib/paginas/home/abas/home_tab.dart
@@ -1,3 +1,4 @@
+import 'package:controle_pressao_arterial/paginas/home/componentes/configuracao_widget.dart';
 import 'package:flutter/material.dart';
 
 import '../../../componentes/espacamento_componente.dart';
@@ -10,124 +11,91 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
-  final TextEditingController pressaoPacienteMax = TextEditingController();
-  final TextEditingController pressaoPacienteMin = TextEditingController();
-
-  final TextEditingController riscoMax = TextEditingController();
-  final TextEditingController riscoMin = TextEditingController();
-
-  @override
-  void dispose() {
-    pressaoPacienteMin.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(8),
+      color: Colors.green,
       child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        const Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              radius: 30,
-              child: Text('JR'),
-            ),
-            EspacamentoComponente(
-              size: 20,
-              isHorizontal: true,
-            ),
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Text('Junior Fonseca',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800)),
-              Text('29 anos',
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200)),
-            ])
-          ],
-        ),
-        const EspacamentoComponente(),
         Card(
-          margin: const EdgeInsets.all(0),
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-            child: Column(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            child: Row(
               children: [
-                const Text('Controle de Pressão Arterial',
-                    style:
-                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                const EspacamentoComponente(),
-                Row(
-                  children: [
-                    const Text('Pressão do Paciente: '),
-                    const EspacamentoComponente(size: 20, isHorizontal: true),
-                    Flexible(
-                      child: SizedBox(
-                        width: 50,
-                        height: 30,
-                        child: TextField(
-                          cursorHeight: 20,
-                          controller: pressaoPacienteMax,
-                        ),
-                      ),
-                    ),
-                    const EspacamentoComponente(size: 20, isHorizontal: true),
-                    Flexible(
-                      child: SizedBox(
-                        width: 50,
-                        height: 30,
-                        child: TextField(
-                          controller: pressaoPacienteMin,
-                        ),
-                      ),
-                    ),
-                    const EspacamentoComponente(
-                        size: 30, isHorizontal: true, isFull: true),
-                  ],
+                const CircleAvatar(
+                  radius: 30,
+                  child: Text('JR'),
                 ),
-                const EspacamentoComponente(),
-                Row(
-                  children: [
-                    const Text('Valor de Risco: '),
-                    const EspacamentoComponente(size: 60, isHorizontal: true),
-                    Flexible(
-                      child: SizedBox(
-                        width: 50,
-                        height: 30,
-                        child: TextField(
-                          controller: riscoMax,
-                        ),
+                const EspacamentoComponente(size: 20, isHorizontal: true),
+                const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Junior Fonseca',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w800),
                       ),
-                    ),
-                    const EspacamentoComponente(size: 20, isHorizontal: true),
-                    Flexible(
-                      child: SizedBox(
-                        width: 50,
-                        height: 30,
-                        child: TextField(
-                          controller: riscoMin,
-                        ),
+                      Text(
+                        '29 anos',
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.w200),
                       ),
-                    ),
-                    const EspacamentoComponente(
-                        size: 30, isHorizontal: true, isFull: true)
-                  ],
-                ),
-                const Divider(color: Colors.grey, thickness: 1),
-                const EspacamentoComponente(),
-                Row(children: [
-                  const Icon(Icons.list),
-                  const SizedBox(width: 8),
-                  const Text('Total de aferições: '),
-                ]),
-                const EspacamentoComponente(),
-                Row(children: [
-                  const Icon(Icons.graphic_eq),
-                  const SizedBox(width: 8),
-                  const Text('Picos: '),
-                ]),
+                    ]),
+                const EspacamentoComponente(isFull: true),
+                IconButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return const SimpleDialog(
+                            contentPadding: EdgeInsets.all(16),
+                            children: [
+                              ConfiguracaoWidget(),
+                            ],
+                          );
+                        });
+                  },
+                  icon: const Icon(Icons.edit_note_rounded),
+                )
               ],
             ),
+          ),
+        ),
+        const EspacamentoComponente(),
+        const Card(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            child: Column(children: [
+              Text('Controle de Pressão Arterial',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              EspacamentoComponente(),
+              Row(
+                children: [
+                  Text('Pressão do Paciente: '),
+                  EspacamentoComponente(size: 20, isHorizontal: true),
+                ],
+              ),
+              EspacamentoComponente(),
+              Row(
+                children: [
+                  Text('Valor de Risco: '),
+                  EspacamentoComponente(size: 60, isHorizontal: true),
+                ],
+              ),
+              Divider(color: Colors.grey, thickness: 1),
+              EspacamentoComponente(),
+              Row(children: [
+                Icon(Icons.list),
+                SizedBox(width: 8),
+                Text('Total de aferições: '),
+              ]),
+              EspacamentoComponente(),
+              Row(children: [
+                Icon(Icons.graphic_eq),
+                SizedBox(width: 8),
+                Text('Picos: '),
+              ]),
+            ]),
           ),
         ),
       ]),
