112d9d7a16105af76ea9a41fe9ac4d8e5acff2ca
Junior <vgfonsecajunior@gmail.com>
2023-07-06 02:07:41 -0300

    config nao persiste

--- lib/services/afazer_service.dart
+++ lib/services/afazer_service.dart
@@ -7,18 +7,11 @@ import '../entities/afazer_entity.dart';
 class AfazerService {
   final _key = 'KEY_AFAZER';
 
-//Future<void> salvar(List<AfazerEntity> itens) async {
-//  SharedPreferences prefs = await SharedPreferences.getInstance();
-//  String listJson =
-//      jsonEncode(itens.map((item) => item.toJson()).toList()).toString();
-//  //String listJson = jsonEncode(itens);
-//  await prefs.setString(_key, listJson);
-//}
   Future<void> salvar(List<AfazerEntity> itens) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<Map<String, dynamic>> jsonList =
-        itens.map((item) => item.toJson()).toList();
-    await prefs.setString(_key, jsonEncode(jsonList));
+    //String listJson = jsonEncode(itens.map((item) => item.toJson()).toList()).toString();
+    String listJson = jsonEncode(itens);
+    await prefs.setString(_key, listJson);
   }
 
   Future<List<AfazerEntity>> buscar() async {
@@ -28,11 +21,5 @@ class AfazerService {
       return AfazerEntity.fromJsonList(jsonDecode(listJson));
     }
     return [];
-  }
-
-  Future<void> salvarAfazer(AfazerEntity afazer) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String afazerJson = jsonEncode(afazer.toJson());
-    await prefs.setString(_key, afazerJson);
   }
 }
